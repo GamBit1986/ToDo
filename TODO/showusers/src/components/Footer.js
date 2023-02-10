5c289517fd7a7b98140982e2c29231d78dce7006
GamBit1986 <mazepa86@gmail.com>
2023-02-10 16:54:19 +0300

    homeWork_2

---
+++ TODO/showusers/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>© 2023 ToDo</p>
+        </footer>
+    )
+    }
+
+    
+
+export default Footer
