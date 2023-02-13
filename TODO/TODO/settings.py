c6bba87488875a182e3bfe2a22f1cb6fb10012ab
GamBit1986 <52270313+GamBit1986@users.noreply.github.com>
2023-02-13 11:05:15 +0300

    homeWork_2 (#2)

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",
+    ]
 
 # Application definition
 
@@ -37,13 +40,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
-    'users',
+    "users",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
---
+++ TODO/showusers/src/components/Menu.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const Menu = () => {
+    return (
+        <menu>
+          <table>  
+        <th>
+            Главная
+        </th>
+        <th>
+            О проекте
+        </th>
+        <th>
+            Контакты
+        </th>
+        <th>
+            ToDo
+        </th>
+        </table>            
+    </menu>
+    )
+    }
+
+    
+
+export default Menu
