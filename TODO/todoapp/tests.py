abbea6ef2bf3df6d37f1d4ce199c0bc083f12421
GamBit1986 <mazepa86@gmail.com>
2023-03-01 17:36:18 +0300

    lesson 8 is start

--- TODO/todoapp/tests.py
+++ TODO/todoapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
 
 # Create your tests here.
+
+
+class TestProjectModelViewSet(TestCase):
+
+    def setUp(self) -> None:
+        return super().setUp()
