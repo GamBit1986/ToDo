b65105ba1e6d169233ebaa8279f7e13331fbb271
GamBit1986 <mazepa86@gmail.com>
2023-03-03 14:21:30 +0300

    lesson 9

--- TODO/APIapp/views.py
+++ TODO/APIapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import UserFullModelSerializer, UserModelSerializer
+
+class UserListAPIView(ListAPIView):
+    queryset = User.object.all()
+
+
+
+    def get_serializer_class(self):
+        if self.request.version == 'V2':
+            return UserFullModelSerializer
+        return UserModelSerializer
